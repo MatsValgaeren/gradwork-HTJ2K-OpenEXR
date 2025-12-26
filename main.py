daa22b9a79c9e9cf1e10b57ff6346e96f956d376
matsv <dev@matsvalgaeren.com>
2025-12-26 20:38:34 +0100

    - Improved timing - Added CPU and RAM usage test

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import shutil
 
 import OpenImageIO as oiio
 import numpy as np
+import platform
+import psutil
 
 import faulthandler
 faulthandler.enable()
@@ -32,11 +34,14 @@ compression_map = {
     'HTJ2K256_COMPRESSION': 'htj2k'
 }
 
+os.environ['OMP_NUM_THREADS'] = '1'
+
 image_dir = "./images"
 temp_dir = "./temp"
 results = []
-result_dir = "./results"
 passes = 10
+result_dir = "./results"
+pc_usage = {}
 
 def remove_files(dir):
     for root, dirs, files in os.walk(dir):
@@ -46,7 +51,7 @@ def remove_files(dir):
             # os.remove(file_path)
 
 @profile
-def go_over_files(compression_method):
+def go_over_files(num_threads, compression_method):
     start = time.perf_counter()
     compression_name = compression_method.split("_")[0]
     for root, dirs, files in os.walk(image_dir):
@@ -61,6 +66,7 @@ def go_over_files(compression_method):
                 oiio_comp = compression_map.get(compression_method)
 
                 compression_res = recompress_oiio.recompress(old_file_path, new_file_path, compression=oiio_comp, passes=passes)
+
 
                 res = {'copy': [], 'read': []}
 
@@ -82,20 +88,32 @@ def go_over_files(compression_method):
                     s_read = time.perf_counter()
                     oiio.ImageBuf(new_file_path)
                     res['read'].append((time.perf_counter()  - s_read) * 1000)  # ms
+                    cpu_usage = psutil.cpu_percent()
+                    ram_usage = psutil.virtual_memory()
 
                 dur_read = np.mean(res['read'])
                 dur_copy = np.mean(res['copy'])
 
-                results.append({"compression": compression_method,"file": filename[:-7], "read_duration": dur_read, "write_duration": compression_res['compress_write_ms'], "file_size": compression_res['size_kb'], "copy": dur_copy})
+                results.append({"processor": platform.processor(),
+                                "num_threads": num_threads,
+                                "cpu_ms": cpu_usage,
+                                "ram_usage": ram_usage[2],
+                                "method": compression_method,
+                                "file": filename[:-7],
+                                "read_ms": dur_read,
+                                "write_ms": compression_res['write_ms'],
+                                "wall_ms": compression_res['wall_ms'],
+                                "size_kb": compression_res['size_kb'],
+                                "copy": dur_copy})
 
-def go_over_compression():
+def go_over_compression(num_threads):
     for compression in compression_map:
-        go_over_files(compression)
+        go_over_files(num_threads, compression)
     # go_over_files('PIZ_COMPRESSION')
 
     remove_files(temp_dir)
 
-    result_file = result_dir + '/data_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.json'
+    result_file = result_dir + '/file-data_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.json'
     # json.dump(results, result_file)
 
 
@@ -103,23 +121,8 @@ def go_over_compression():
         json.dump(results, f, ensure_ascii=False, indent=4)
 
 
-go_over_compression()
-# print(results)
+oiio.attribute("threads", 1)
+go_over_compression(num_threads = 1)
 
-import OpenImageIO as oiio
-import numpy as np
-
-import os
-import OpenEXR
-
-for root, dirs, files in os.walk(temp_dir):
-    for filename in files:
-        print('start')
-        file_path = os.path.join(root, filename)
-        print('filepath ', file_path)
-
-        infile = OpenEXR.InputFile(file_path)
-        header = infile.header()
-        comp = header['compression']
-        print(f"Compression: {comp}")
-        infile.close()
+oiio.attribute("threads", os.cpu_count())
+go_over_compression(num_threads = os.cpu_count())
--- recompress_oiio.py
+++ recompress_oiio.py
@@ -2,10 +2,10 @@ import numpy as np
 import OpenImageIO as oiio
 import time
 import os
+import psutil
 
 def recompress(in_path, out_path, compression='none', passes=1):
     out_path = str(out_path)
-
     buf = oiio.ImageBuf(in_path)
 
     spec = buf.spec().copy()
@@ -13,31 +13,37 @@ def recompress(in_path, out_path, compression='none', passes=1):
 
     pixels = buf.get_pixels(buf.spec().format)
 
-    # results = {'compress': [], 'write': []}
-    times = []
+    process = psutil.Process()
+    start_cpu = process.cpu_times()
+
+    write_times = []
+
+    out = oiio.ImageOutput.create(out_path)
+    out.open(out_path, spec)
 
     for _ in range(passes):
         out = oiio.ImageOutput.create(out_path)
         out.open(out_path, spec)
 
-        # s_compress = time.perf_counter()
-        # compressed_buf = oiio.ImageBufAlgo.compression(pixels, compression)
-        # e_compress = time.perf_counter()
-        # time_to_compress = e_compress - s_compress
-
         s_write = time.perf_counter()
-        out.write_image(pixels)
+        ok = out.write_image(pixels)
         e_write = time.perf_counter()
-
         out.close()
-        # results['compress'].append((e_compress - s_compress) * 1000)  # ms
-        times.append((e_write - s_write) * 1000)
-        # results['write'].append()  # ms
 
+        if ok:
+            write_times.append((e_write - s_write) * 1000)
+
+    end_cpu = process.cpu_times()
+    cpu_ms = ((end_cpu.user + end_cpu.system) -
+              (start_cpu.user + start_cpu.system)) / max(passes, 1) * 1000
+
+    mean_write_ms = np.mean(write_times) if write_times else float('nan')
 
     return {
-        'compress_write_ms': np.mean(times),
-        # 'write_ms': np.mean(results['write']),
-        'std_ms': np.std(times),
+        'compress_write_ms': mean_write_ms,  # total wall time
+        'write_ms': mean_write_ms,
+        'total_cpu_ms': cpu_ms,
+        'wall_ms': mean_write_ms,
         'size_kb': os.path.getsize(out_path) / 1024
+        if os.path.exists(out_path) else 0
     }
